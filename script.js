091f7048eecf9abb81bfa3ac678da2792b7d6447
Yehoyakim88 <yehoyakim@mail.de>
2022-11-05 20:45:11 +0100

    interlinear layout added

--- script.js
+++ script.js
@@ -4,34 +4,60 @@ async function fetchBible() {
   let verse;
   let verseArray;
   let verseArrayReverse;
-  let verseArrayReverseRejoined;
+  let transliterationReverse;
+  let germanBible;
+  let germanBibleAsJson;
+  let germanVerse;
+  let germanVerseAsArray;
+  let germanVerseAsArrayReverse;
 
   let transliteration = 'wə•ḡam ʾeṯ-šab•bə•ṯōw•ṯay nā•ṯat•tiy lā•hem lih•yōwṯ lə•ʾōwṯ bēy•niy uw•ḇēy•nēy•hem lā•ḏa•ʿaṯ kiy ʾă•niy yəh•wāh mə•qad•də•šām';
-  //   transliteration.replace('\xe2\x80\xa2', 'bbbbbbbbb');
-  
-  for (let index = 0; index < 100; index++) {
-    transliteration = transliteration.replace("•", "");
-  }
-  
   transliteration = transliteration.replace("-", " ");
+  transliteration = transliteration.split(" ");
+  transliterationReverse = transliteration.reverse();
+//   transliteration = cleanTransliteration(transliteration);
 
-  console.log(bibleFileAsJson);
-  console.log(verse);
+  
 
   bibleFile = await fetch("./wlc.json");
   bibleFileAsJson = await bibleFile.json();
   verse = bibleFileAsJson["books"][13]["chapters"][19]["verses"][11]["text"];
-
   verse = replaceSpecialChars(verse);
   verseArray = verseAsSingleWordsArray(verse);
   verseArrayReverse = verseArray.reverse();
-  verseArrayReverseRejoined = verseArrayReverse.join(" ");
 
-  document.getElementById("text-displayer").innerHTML += /*html*/ `
-      <h1>Verse as Array:</h1>
-      <br><span class="verse-Ezra-SIL">${verseArrayReverseRejoined}</span>
-      <br><span class="transliterated">${transliteration}</span>
-      `;
+  germanBible = await fetch("./schlachter.json");
+  germanBibleAsJson = await germanBible.json();
+  germanVerse = germanBibleAsJson["books"][25]["chapters"][19]["verses"][11]["text"];
+  germanVerseAsArray = verseAsSingleWordsArray(germanVerse);
+  germanVerseAsArrayReverse = germanVerseAsArray.reverse();
+
+  console.log(germanBibleAsJson);
+//   console.log(verse);
+
+  
+  
+
+  for(let i = verseArrayReverse.length-1; i > 0; i--) {
+    document.getElementById("content").innerHTML += /*html*/ `
+    <div id="word-block-${i}" class="wordBlock">
+        <div class="verse-Ezra-SIL">${verseArrayReverse[i]}</div>
+        <div class="transliterated">${transliteration[i-1]}</div>
+        <div class="wordStrongs">STRONG's</div>
+    </div>
+    `;
+  }
+  for(let i = germanVerseAsArrayReverse.length-1; i > 0; i--) {
+    document.getElementById(`word-block-${i-9}`).innerHTML += /*html*/ `
+    <div class="interlinearGerman">${germanVerseAsArrayReverse[i]}</div>
+  `;
+  }
+
+//   document.getElementById("text-displayer").innerHTML += /*html*/ `
+//       <h1>Verse as Array:</h1>
+//       <br><span class="verse-Ezra-SIL">${verseArrayReverseRejoined}</span>
+//       <br><span class="transliterated">${transliteration}</span>
+//       `;
 }
 
 async function display_wlc_SBL_Hebrew() {
@@ -56,9 +82,20 @@ function replaceSpecialChars(myString) {
   return myString.replace("\u05be", " ");
 }
 
+
+function cleanTransliteration(givenTransliteration) {
+    // for (let index = 0; index < 100; index++) {
+    //     givenTransliteration.replace("•", "");
+    // }
+    givenTransliteration.replace("-", " ");
+    return givenTransliteration;
+}
+
+
 function verseAsSingleWordsArray(givenVerse) {
   return givenVerse.split(" ");
 }
+
 
 function testFunction() {
   document.getElementById("text-displayer").innerHTML += /*html*/ `
